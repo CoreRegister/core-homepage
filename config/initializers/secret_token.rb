e945cd655d26685bac8d38c4c5e15905e5b622ef
Christopher Morton <ssnacks@gmail.com>
2014-01-13 23:32:32 -0800

    removed git garbage

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 Core::Application.config.secret_token = ENV['SECRET_TOKEN']
-=======
-Core::Application.config.secret_token = ENV['SECRET_TOKEN']
->>>>>>> 3d0278b46f8b116c678165a4219087bf90f2c1a8
