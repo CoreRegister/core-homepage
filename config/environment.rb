00ebd0fcc0e45dd9e21d6dac878480b072bfc2bb
Christopher Morton <ssnacks@gmail.com>
2014-01-18 13:37:57 -0800

    switched to environment edits

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,13 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 Core::Application.configure do
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-:address              => "smtp.sendgrid.net",
-:port                 => 587,
-:domain               => "heroku.com",
-:user_name            => ENV['SENDGRID_USERNAME'],
-:password             => ENV['SENDGRID_PASSWORD'],
-:authentication       => 'plain',
-:enable_starttls_auto => true  }
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
