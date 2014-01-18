73ce2f7d7ab8d20229af74e23ec1aaa507196b1c
Christopher Morton <ssnacks@gmail.com>
2014-01-18 13:33:58 -0800

    switched to sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Rails.application.initialize!
 Core::Application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-:address              => "smtp.gmail.com",
+:address              => "smtp.sendgrid.net",
 :port                 => 587,
-:domain               => "coreregister.com",
-:user_name            => "chris@coreregister.com",
-:password             => "89fb918a9ddcc475fe5668d542e3c2c6",
+:domain               => "heroku.com",
+:user_name            => ENV['SENDGRID_USERNAME'],
+:password             => ENV['SENDGRID_PASSWORD'],
 :authentication       => 'plain',
 :enable_starttls_auto => true  }
 config.action_mailer.perform_deliveries = true
